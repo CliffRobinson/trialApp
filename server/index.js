4f892380e6bf44bf4d811723c624bc8550a408d0
rikiparata <rikiipee47@gmail.com>
2018-07-16 19:09:14 +1200

    sweetness

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const server = require('./server')
 const port = 3000
-
+const beNice = require('../../terminal-sweetness')
 
 server.listen(port, ()=> {
     console.log(`Listening on port ${port}, time is ${new Date}`)
+    beNice();
 })
+//ch
